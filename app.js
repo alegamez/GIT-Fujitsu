9fe68908e2084a8df5f6373d623781590f292f51
alegamez <alegamez7@gmail.com>
2024-03-12 20:02:39 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+// Definir una función para saludar
+function saludar(nombre) {
+    console.log("¡Hola, " + nombre + "! Bienvenido.");
+  }
+  
+  // Llamar a la función con un nombre específico
+  saludar("Juan");
+  
