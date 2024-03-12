9a13abb600446a01d390acf4be04c0027ac032f0
alegamez <alegamez7@gmail.com>
2024-03-12 20:04:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,48 @@ function saludar(nombre) {
     console.log("¡Hola, " + nombre + "! Bienvenido.");
   }
   
+  // Definir un objeto para manejar las tareas
+  const gestorTareas = {
+    tareas: [],
+    
+    // Método para agregar una tarea
+    agregarTarea: function(tarea) {
+      this.tareas.push(tarea);
+      console.log("Tarea agregada:", tarea);
+    },
+    
+    // Método para mostrar todas las tareas
+    mostrarTareas: function() {
+      console.log("Tareas:");
+      this.tareas.forEach(function(tarea, indice) {
+        console.log(indice + 1 + ". " + tarea);
+      });
+    },
+    
+    // Método para completar una tarea
+    completarTarea: function(indice) {
+      if (indice >= 0 && indice < this.tareas.length) {
+        this.tareas.splice(indice, 1);
+        console.log("Tarea completada.");
+      } else {
+        console.log("Índice de tarea no válido.");
+      }
+    }
+  };
+  
   // Llamar a la función con un nombre específico
   saludar("Juan");
+  
+  // Agregar algunas tareas
+  gestorTareas.agregarTarea("Hacer la compra");
+  gestorTareas.agregarTarea("Llamar al cliente");
+  
+  // Mostrar todas las tareas
+  gestorTareas.mostrarTareas();
+  
+  // Completar una tarea
+  gestorTareas.completarTarea(0);
+  
+  // Mostrar todas las tareas después de completar una
+  gestorTareas.mostrarTareas();
   
